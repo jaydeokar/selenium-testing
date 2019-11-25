6fdba1cecbeb5b5599e84c48ab8141d18f700528
Jay Deokar <deokarjay215@gmail.com>
2019-11-25 00:43:29 -0500

    Update mm_login_test.py

--- mm_login_test.py
+++ mm_login_test.py
@@ -7,7 +7,16 @@ class GithubWatchRepoTest(unittest.TestCase):
 
         def setUp(self):
             self.teamname = "csc-510-f19"
-            self.driver = webdriver.PhantomJS()
+            chromeOptions = webdriver.ChromeOptions()
+            chromeOptions.add_argument("--no-sandbox")
+            chromeOptions.add_argument("--disable-setuid-sandbox")
+            chromeOptions.add_argument("--disable-dev-shm-using")
+            chromeOptions.add_argument("--disable-extensions")
+            chromeOptions.add_argument("--disable-gpu")
+            chromeOptions.add_argument("start-maximized")
+            chromeOptions.add_argument("disable-infobars")
+            chromeOptions.add_argument("--headless")
+            self.driver = webdriver.Chrome(chrome_options=chromeOptions)
             self.url = "http://34.66.232.72:8065/login"
             self.driver.get(self.url)
             sleep(10)
